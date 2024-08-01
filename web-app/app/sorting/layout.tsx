d71729b7982983c5f40d6101a062a4a1769208c4
PoowadolDev <poowadol.dev@gmail.com>
2024-08-01 23:51:17 +0700

    feat(sort-page): add nav bar

--- web-app/app/sorting/layout.tsx
+++ web-app/app/sorting/layout.tsx
@@ -1,9 +1,27 @@
-// Create layout for sorting page navigation bar
+import Link from "next/link"
+
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
     <>
-        <div className="navbar bg-base-100">
-            <a className="btn btn-ghost text-xl">daisyUI</a>
+        <div className="navbar bg-slate-700">
+            <a className="btn btn-ghost text-3xl">Sorting</a>
+            <Link href="/" className="font-normal ps-4 text-xl">Home</Link>
+            <a className="font-normal ps-6 text-xl">Random</a>
+            <div className="flex-none">
+                <ul className="menu menu-horizontal">
+                    <li>
+                        <details>
+                            <summary className="text-xl">Algorithm</summary>
+                                <ul className="bg-base-100 rounded-t-none w-36 font-semibold">
+                                    <li><a>Selection Sort</a></li>
+                                    <li><a>Merge Sort</a></li>
+                                    <li><a>Insertion Sort</a></li>
+                                    <li><a>Bubble Sort</a></li>
+                                </ul>
+                        </details>
+                    </li>
+                </ul>
+            </div>
         </div>
         <div>{children}</div>
     </>
